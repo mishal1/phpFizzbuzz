c4775da12fbf5d8f29065f32f2c59e13c3b7d2a0
Mishal Islam <mishal.islam01@gmail.com>
2015-04-14 12:36:32 +0100

    fizzbuzz

--- lib/fizzbuzz.php
+++ lib/fizzbuzz.php
@@ -1,1 +1,28 @@
 <?php
+
+class Fizzbuzz{
+
+  public function divisibleByThree($number){
+    return $this->divisibleBy(3, $number);
+  }
+
+  public function divisibleByFive($number){
+    return $this->divisibleBy(5, $number);
+  }
+
+  public function divisibleByFifteen($number){
+    return $this->divisibleBy(15, $number);
+  }
+
+  public function divisibleBy($divisor, $number){
+    return $number % $divisor == 0;
+  }
+
+  public function says($number){
+    if($this->divisibleByFifteen($number)) return 'Fizzbuzz';
+    if($this->divisibleByFive($number)) return 'Buzz';
+    if($this->divisibleByThree($number)) return 'Fizz';
+    return $number;
+  }
+
+}
--- tests/fizzbuzzTests.php
+++ tests/fizzbuzzTests.php
@@ -1,7 +1,78 @@
 <?php
 
-require 'lib/fizzbuzz.php'
+require 'lib/fizzbuzz.php';
 
-class FizzbuzzTest extends PHPUnit_Framework_TestCase {
+class FizzbuzzTest extends PHPUnit_Framework_TestCase{
+  
+  public function __construct(){
+    $this->fizzbuzz = new Fizzbuzz();
+  }
+
+  public function testDivisibleByThree(){
+    $actual = $this->fizzbuzz->divisibleByThree(3);
+    $this->assertTrue($actual);
+  }
+
+  public function testNotDivisibleByThree(){
+    $actual = $this->fizzbuzz->divisibleByThree(1);
+    $this->assertFalse($actual);
+  }
+
+  public function testDivisibleByFive(){
+    $actual = $this->fizzbuzz->divisibleByFive(5);
+    $this->assertTrue($actual);
+  }
+
+  public function testNotDivisibleByFive(){
+    $actual = $this->fizzbuzz->divisibleByFive(1);
+    $this->assertFalse($actual);
+  }
+
+  public function testDivisibleByFifteen(){
+    $actual = $this->fizzbuzz->divisibleByFifteen(15);
+    $this->assertTrue($actual);
+  }
+
+  public function testNotDivisibleByFifteen(){
+    $actual = $this->fizzbuzz->divisibleByFifteen(1);
+    $this->assertFalse($actual);
+  }
+
+  public function testSaysFizz(){
+    $expected = 'Fizz';
+    $actual = $this->fizzbuzz->says(3);
+    $this->assertEquals($expected, $actual);
+  }
+
+  public function testSaysBuzz(){
+    $expected = 'Buzz';
+    $actual = $this->fizzbuzz->says(5);
+    $this->assertEquals($expected, $actual);
+  }
+
+  public function testSaysFizzBuzz(){
+    $expected = 'Fizzbuzz';
+    $actual = $this->fizzbuzz->says(15);
+    $this->assertEquals($expected, $actual);
+  }
+
+  public function testSaysNumber(){
+    $expected = 1;
+    $actual = $this->fizzbuzz->says(1);
+    $this->assertEquals($expected, $actual);
+  }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
